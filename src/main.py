1f9684315aa40a24b90344579d072705ac675519
junghoonkim <joseph.junghoon.kim@gmail.com>
2020-01-29 00:58:45 +0900

    Add link

--- src/main.py
+++ src/main.py
@@ -26,33 +26,32 @@ def find_tag(item, tag):
     except AttributeError:
         return None
 
-    result = re.sub('<table.*?>.*?</table>', "", result, 0, re.I | re.S)
+    if tag is 'dc:date':
+        return change_datetime(result)
+    elif tag is 'description':
+        result = re.sub('<table.*?>.*?</table>', "", result, 0, re.I | re.S)
     return result.strip()
 
 
-URLS = ['http://www.chosun.com/site/data/rss/rss.xml',  # 조선일보
-        'http://rss.nocutnews.co.kr/nocutnews.xml',  # 노컷뉴스
-        'http://www.khan.co.kr/rss/rssdata/total_news.xml'  # 경향신문
-        ]
+URLS = {'조선일보': 'http://www.chosun.com/site/data/rss/rss.xml',
+        '동아일보': 'https://rss.donga.com/total.xml',
+        '노컷뉴스': 'http://rss.nocutnews.co.kr/nocutnews.xml',
+        '경향신문': 'http://www.khan.co.kr/rss/rssdata/total_news.xml'}
 
-markup = get_html(URLS[0])
-soup = bs(markup, 'html.parser')
-
-news = soup.find('title').text
+markup = get_html(URLS['조선일보'])
+soup = bs(markup, 'lxml-xml')
 
 news_item = soup.find('item')
 news_title = find_tag(news_item, 'title')
 news_description = find_tag(news_item, 'description')
 news_author = find_tag(news_item, 'author')
+news_link = find_tag(news_item, 'link')
 news_date = find_tag(news_item, 'dc:date')
 news_date = change_datetime(news_date)
-
-news_link = news_item.find('link')
 
 print(news_item)
 print()
 print()
-print(news)
 print(news_title)
 print(news_description)
 print(news_author)
