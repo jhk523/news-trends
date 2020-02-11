fd14cff4afdb3e2c9787c273bc7a914df63024cf
joseph <joseph.junghoon.kim@gmail.com>
2020-02-11 14:48:53 +0900

    Add duplicates function

--- src/newstrends/data/db/mysql.py
+++ src/newstrends/data/db/mysql.py
@@ -17,3 +17,20 @@ _URL = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}?charset=utf8mb4'.format(
     _USER, _PASSWORD, _ADDRESS, _PORT, _DB)
 ENGINE = create_engine(_URL, echo=False, encoding='utf-8', pool_recycle=3600)
 
+
+def create_news_table():
+    query = "create table if not exists news(" \
+            "`date` DATETIME not null, " \
+            "publisher VARCHAR(255), " \
+            "title VARCHAR(255), " \
+            "author VARCHAR(255), " \
+            "link VARCHAR(255) UNIQUE, " \
+            "description TEXT)"
+    ENGINE.execute(query)
+
+
+def read_publisher_links(publisher):
+    query = "select link from news where publisher='{}'"
+    fetched = ENGINE.execute(query.format(publisher)).fetchall()
+    df = pd.DataFrame(fetched, columns=['link'])
+    return df.assign(dup=1)
--- src/newstrends/data/scraping/scrape_news.py
+++ src/newstrends/data/scraping/scrape_news.py
@@ -7,33 +7,33 @@ from bs4 import BeautifulSoup as bs
 from newstrends.data.db import mysql
 
 URLS = {'조선일보': 'http://www.chosun.com/site/data/rss/rss.xml',
-        '동아일보': 'https://rss.donga.com/total.xml',
-        '중앙일보': 'https://rss.joins.com/joins_news_list.xml',
-        '노컷뉴스': 'http://rss.nocutnews.co.kr/nocutnews.xml',
-        '경향신문': 'http://www.khan.co.kr/rss/rssdata/total_news.xml',
-        '한겨례': 'http://www.hani.co.kr/rss/',
-        '국민일보': 'http://rss.kmib.co.kr/data/kmibRssAll.xml',
-        '데일리경제': 'http://www.kdpress.co.kr/rss/allArticle.xml',
-        '매일경제': 'https://www.mk.co.kr/rss/40300001/',
-        '머니투데이': 'https://rss.mt.co.kr/mt_news.xml',
-        '세계일보': 'http://www.segye.com/Articles/RSSList/segye_recent.xml',
-        '오마이뉴스': 'http://rss.ohmynews.com/rss/ohmynews.xml',
-        '전자신문': 'http://rss.etnews.com/Section901.xml',
-        '한국경제': 'http://rss.hankyung.com/new/news_main.xml',
-        'SBS정치': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=01&plink=RSSREADER',
-        'SBS경제': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER',
-        'SBS사회': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=03&plink=RSSREADER',
-        'SBS생활/문화': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=07&plink=RSSREADER',
-        'SBS국제/글로벌': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=08&plink=RSSREADER',
-        'SBS연예/방송': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=14&plink=RSSREADER',
-        'SBS스포츠': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=09&plink=RSSREADER',
-        'JTBC정치': 'http://fs.jtbc.joins.com/RSS/politics.xml',
-        'JTBC경제': 'http://fs.jtbc.joins.com/RSS/economy.xml',
-        'JTBC사회': 'http://fs.jtbc.joins.com/RSS/society.xml',
-        'JTBC국제': 'http://fs.jtbc.joins.com/RSS/international.xml',
-        'JTBC문화': 'http://fs.jtbc.joins.com/RSS/culture.xml',
-        'JTBC연예': 'http://fs.jtbc.joins.com/RSS/entertainment.xml',
-        'JTBC스포츠': 'http://fs.jtbc.joins.com/RSS/sports.xml'
+        # '동아일보': 'https://rss.donga.com/total.xml',
+        # '중앙일보': 'https://rss.joins.com/joins_news_list.xml',
+        # '노컷뉴스': 'http://rss.nocutnews.co.kr/nocutnews.xml',
+        # '경향신문': 'http://www.khan.co.kr/rss/rssdata/total_news.xml',
+        # '한겨례': 'http://www.hani.co.kr/rss/',
+        # '국민일보': 'http://rss.kmib.co.kr/data/kmibRssAll.xml',
+        # '데일리경제': 'http://www.kdpress.co.kr/rss/allArticle.xml',
+        # '매일경제': 'https://www.mk.co.kr/rss/40300001/',
+        # '머니투데이': 'https://rss.mt.co.kr/mt_news.xml',
+        # '세계일보': 'http://www.segye.com/Articles/RSSList/segye_recent.xml',
+        # '오마이뉴스': 'http://rss.ohmynews.com/rss/ohmynews.xml',
+        # '전자신문': 'http://rss.etnews.com/Section901.xml',
+        # '한국경제': 'http://rss.hankyung.com/new/news_main.xml',
+        # 'SBS정치': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=01&plink=RSSREADER',
+        # 'SBS경제': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER',
+        # 'SBS사회': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=03&plink=RSSREADER',
+        # 'SBS생활/문화': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=07&plink=RSSREADER',
+        # 'SBS국제/글로벌': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=08&plink=RSSREADER',
+        # 'SBS연예/방송': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=14&plink=RSSREADER',
+        # 'SBS스포츠': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=09&plink=RSSREADER',
+        # 'JTBC정치': 'http://fs.jtbc.joins.com/RSS/politics.xml',
+        # 'JTBC경제': 'http://fs.jtbc.joins.com/RSS/economy.xml',
+        # 'JTBC사회': 'http://fs.jtbc.joins.com/RSS/society.xml',
+        # 'JTBC국제': 'http://fs.jtbc.joins.com/RSS/international.xml',
+        # 'JTBC문화': 'http://fs.jtbc.joins.com/RSS/culture.xml',
+        # 'JTBC연예': 'http://fs.jtbc.joins.com/RSS/entertainment.xml',
+        # 'JTBC스포츠': 'http://fs.jtbc.joins.com/RSS/sports.xml'
         }
 
 
@@ -70,17 +70,6 @@ def find_tag(item, tag):
         return result.strip()
 
 
-def _create_news_table():
-    query = "create table if not exists news(" \
-            "`date` DATETIME not null, " \
-            "publisher VARCHAR(255), " \
-            "title VARCHAR(255), " \
-            "author VARCHAR(255), " \
-            "link VARCHAR(255), " \
-            "description TEXT)"
-    mysql.ENGINE.execute(query)
-
-
 def _create_news_dataframe(init=False, df=None, items=None, pub=None):
     if init:
         columns = ['date', 'title', 'author', 'link', 'description']
@@ -104,6 +93,13 @@ def _create_news_dataframe(init=False, df=None, items=None, pub=None):
         return df
 
 
+def _remove_duplicates(df, publisher):
+    db_links = mysql.read_publisher_links(publisher)
+    merge_df = pd.merge(df, db_links, how='left', on=['link'])
+    merge_df = merge_df[merge_df['dup'] != 1]
+    return merge_df.drop(columns=['dup'])
+
+
 def update_news(initialize, verbose, test):
     if test:
         markup = get_html(URLS['JTBC정치'])
@@ -113,7 +109,7 @@ def update_news(initialize, verbose, test):
         print(soup)
         return
     if initialize:
-        _create_news_table()
+        mysql.create_news_table()
 
     for publisher in URLS.keys():
         if verbose:
@@ -125,4 +121,6 @@ def update_news(initialize, verbose, test):
         news_item = soup.find_all('item')
         news_df = _create_news_dataframe(df=news_df, items=news_item,
                                          pub=publisher)
+        if not initialize:
+            news_df = _remove_duplicates(news_df, publisher)
         news_df.to_sql('news', mysql.ENGINE, if_exists='append', index=False)
