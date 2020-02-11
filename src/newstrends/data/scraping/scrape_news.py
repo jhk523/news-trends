6aa61d691380103aff5b79acece5350e4821e4c0
junghoonkim <joseph.junghoon.kim@gmail.com>
2020-02-11 23:58:47 +0900

    Fix bug

--- src/newstrends/data/scraping/scrape_news.py
+++ src/newstrends/data/scraping/scrape_news.py
@@ -6,34 +6,34 @@ from bs4 import BeautifulSoup as bs
 
 from newstrends.data.db import mysql
 
-URLS = {'조선일보': 'http://www.chosun.com/site/data/rss/rss.xml',
-        '동아일보': 'https://rss.donga.com/total.xml',
-        '중앙일보': 'https://rss.joins.com/joins_news_list.xml',
-        '노컷뉴스': 'http://rss.nocutnews.co.kr/nocutnews.xml',
-        '경향신문': 'http://www.khan.co.kr/rss/rssdata/total_news.xml',
-        '한겨례': 'http://www.hani.co.kr/rss/',
-        '국민일보': 'http://rss.kmib.co.kr/data/kmibRssAll.xml',
-        '데일리경제': 'http://www.kdpress.co.kr/rss/allArticle.xml',
-        '매일경제': 'https://www.mk.co.kr/rss/40300001/',
-        '머니투데이': 'https://rss.mt.co.kr/mt_news.xml',
-        '세계일보': 'http://www.segye.com/Articles/RSSList/segye_recent.xml',
-        '오마이뉴스': 'http://rss.ohmynews.com/rss/ohmynews.xml',
-        '전자신문': 'http://rss.etnews.com/Section901.xml',
-        '한국경제': 'http://rss.hankyung.com/new/news_main.xml',
-        'SBS정치': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=01&plink=RSSREADER',
-        'SBS경제': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER',
-        'SBS사회': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=03&plink=RSSREADER',
-        'SBS생활/문화': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=07&plink=RSSREADER',
-        'SBS국제/글로벌': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=08&plink=RSSREADER',
-        'SBS연예/방송': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=14&plink=RSSREADER',
-        'SBS스포츠': 'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=09&plink=RSSREADER',
-        'JTBC정치': 'http://fs.jtbc.joins.com/RSS/politics.xml',
-        'JTBC경제': 'http://fs.jtbc.joins.com/RSS/economy.xml',
-        'JTBC사회': 'http://fs.jtbc.joins.com/RSS/society.xml',
-        'JTBC국제': 'http://fs.jtbc.joins.com/RSS/international.xml',
-        'JTBC문화': 'http://fs.jtbc.joins.com/RSS/culture.xml',
-        'JTBC연예': 'http://fs.jtbc.joins.com/RSS/entertainment.xml',
-        'JTBC스포츠': 'http://fs.jtbc.joins.com/RSS/sports.xml'
+URLS = {'조선일보': ['http://www.chosun.com/site/data/rss/rss.xml'],
+        '동아일보': ['https://rss.donga.com/total.xml'],
+        '중앙일보': ['https://rss.joins.com/joins_news_list.xml'],
+        '노컷뉴스': ['http://rss.nocutnews.co.kr/nocutnews.xml'],
+        '경향신문': ['http://www.khan.co.kr/rss/rssdata/total_news.xml'],
+        '한겨례': ['http://www.hani.co.kr/rss/'],
+        '국민일보': ['http://rss.kmib.co.kr/data/kmibRssAll.xml'],
+        '데일리경제': ['http://www.kdpress.co.kr/rss/allArticle.xml'],
+        '매일경제': ['https://www.mk.co.kr/rss/40300001/'],
+        '머니투데이': ['https://rss.mt.co.kr/mt_news.xml'],
+        '세계일보': ['http://www.segye.com/Articles/RSSList/segye_recent.xml'],
+        '오마이뉴스': ['http://rss.ohmynews.com/rss/ohmynews.xml'],
+        '전자신문': ['http://rss.etnews.com/Section901.xml'],
+        '한국경제': ['http://rss.hankyung.com/new/news_main.xml'],
+        'SBS': ['https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=01&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=02&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=03&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=07&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=08&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=14&plink=RSSREADER',
+                'https://news.sbs.co.kr/news/SectionRssFeed.do?sectionId=09&plink=RSSREADER'],
+        'JTBC': ['http://fs.jtbc.joins.com/RSS/politics.xml',
+                 'http://fs.jtbc.joins.com/RSS/economy.xml',
+                 'http://fs.jtbc.joins.com/RSS/society.xml',
+                 'http://fs.jtbc.joins.com/RSS/international.xml',
+                 'http://fs.jtbc.joins.com/RSS/culture.xml',
+                 'http://fs.jtbc.joins.com/RSS/entertainment.xml',
+                 'http://fs.jtbc.joins.com/RSS/sports.xml']
         }
 
 
@@ -114,15 +114,18 @@ def update_news(initialize, verbose, test):
     for publisher in URLS.keys():
         if verbose:
             print(publisher)
-        markup = get_html(URLS[publisher])
 
         news_df = _create_news_dataframe(init=True)
-        soup = bs(markup, 'lxml-xml')
-        news_item = soup.find_all('item')
-        news_df = _create_news_dataframe(df=news_df, items=news_item,
-                                         pub=publisher)
-        if not initialize:
-            news_df = _remove_duplicates(news_df, publisher)
+
+        for sub_section in URLS[publisher]:
+            markup = get_html(sub_section)
+
+            soup = bs(markup, 'lxml-xml')
+            news_item = soup.find_all('item')
+            news_df = _create_news_dataframe(df=news_df, items=news_item,
+                                             pub=publisher)
+            if not initialize:
+                news_df = _remove_duplicates(news_df, publisher)
 
         news_df.drop_duplicates(inplace=True, keep=False, subset=['link'])
         news_df.to_sql('news', mysql.ENGINE, if_exists='append', index=False)
