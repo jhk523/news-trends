8f5e0a3b04e129ce72479c7fc8c4feabd30e7fe2
Jaemin Yoo <jaeminyoo@snu.ac.kr>
2020-02-19 13:48:13 +0900

    Update the replacement dictionary

--- src/newstrends/utils.py
+++ src/newstrends/utils.py
@@ -4,10 +4,13 @@ import re
 def preprocess(articles):
     stopwords = ['&#039;', '&quot;', '<span>', '</span>', '<span id="divTitle">',
                  '</font>', '<b>', '</b>']
+    replace_dict = {'···': '…', '...': '…', '..': '…'}
     new_articles = []
     for article in articles:
         for word in stopwords:
             article = article.replace(word, '')
+        for word, replace in replace_dict.items():
+            article = article.replace(word, replace)
         article = re.sub(' +', ' ', article)
         if article.startswith(' '):
             article = article[1:]
