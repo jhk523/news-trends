056036a2fd27f9df846e2e6be01ec018476a539e
Jaemin Yoo <jaeminyoo@snu.ac.kr>
2020-02-20 11:09:16 +0900

    Improve the models

--- src/newstrends/models.py
+++ src/newstrends/models.py
@@ -2,6 +2,8 @@ import typing
 
 import torch
 from torch import nn
+from torch.nn import LayerNorm
+from torch.nn.modules.transformer import TransformerEncoderLayer, TransformerEncoder
 
 
 class SoftmaxClassifier(nn.Module):
@@ -19,12 +21,19 @@ class SoftmaxClassifier(nn.Module):
         return self.linear(out)
 
 
-class GRUClassifier(nn.Module):
-    def __init__(self, vocab_size, num_classes, embedding_dim=8, hidden_size=8):
+class RNNClassifier(nn.Module):
+    def __init__(self, vocab_size, num_classes, embedding_dim=8, hidden_size=8, cell_type='lstm'):
         super().__init__()
+        assert cell_type in {'gru', 'lstm'}
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.gru = nn.GRU(embedding_dim, hidden_size)
-        self.linear = nn.Linear(hidden_size, num_classes)
+        if cell_type == 'lstm':
+            self.rnn = nn.LSTM(embedding_dim, hidden_size)
+        else:
+            self.rnn = nn.GRU(embedding_dim, hidden_size)
+        self.dense = nn.Sequential(
+            nn.Linear(hidden_size, 2 * hidden_size),
+            nn.ReLU(),
+            nn.Linear(2 * hidden_size, num_classes))
 
     # noinspection PyShadowingBuiltins
     def __call__(self, *input, **kwargs) -> typing.Any:
@@ -36,5 +45,34 @@ class GRUClassifier(nn.Module):
         out = self.embedding.weight.index_select(dim=0, index=out.view(-1))
         out = out.view(x_t.size(0), x_t.size(1), -1)
         out.masked_fill_((x_t < 0).unsqueeze(dim=2), value=0)
-        out = self.gru(out)[-1].squeeze(dim=0)
-        return self.linear(out)
+        out = self.rnn(out)[0][-1]
+        return self.dense(out)
+
+
+class TransformerClassifier(nn.Module):
+    def __init__(self, vocab_size, num_classes, embedding_dim=8):
+        super().__init__()
+        d_model = embedding_dim
+        nhead = 1
+        dim_feedforward = 2 * d_model
+        num_encoder_layers = 2
+
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        encoder_layer = TransformerEncoderLayer(d_model, nhead, dim_feedforward)
+        encoder_norm = LayerNorm(d_model)
+        self.encoder = TransformerEncoder(
+            encoder_layer, num_encoder_layers, encoder_norm)
+        self.linear = nn.Linear(d_model, num_classes)
+
+    # noinspection PyShadowingBuiltins
+    def __call__(self, *input, **kwargs) -> typing.Any:
+        return super().__call__(*input, **kwargs)
+
+    def forward(self, x):
+        x_t = x.transpose(0, 1)
+        out = torch.clamp(x_t, min=0)
+        out = self.embedding.weight.index_select(dim=0, index=out.view(-1))
+        out = out.view(x_t.size(0), x_t.size(1), -1)
+        src_key_padding_mask = x_t < 0
+        out = self.encoder(out, src_key_padding_mask=src_key_padding_mask)
+        return self.linear(out[0])
--- src/scripts/sentiment.py
+++ src/scripts/sentiment.py
@@ -18,11 +18,11 @@ def read_reviews():
 def train_classifier(cls_model, spm_model, vocab):
     reviews, labels = read_reviews()
     pieces = [spm_model.encode_as_pieces(r) for r in reviews]
-    features = utils.to_integer_matrix(pieces, vocab)
+    features = utils.to_integer_matrix(pieces, vocab, padding='first')
     dataset = TensorDataset(features, torch.tensor(labels))
     loader = DataLoader(dataset, batch_size=2048, shuffle=True)
     utils.train_model(
-        cls_model, loader, lr=1e-4, print_every=5, num_epochs=100)
+        cls_model, loader, lr=1e-4, print_every=1, num_epochs=100)
 
 
 def start_interactive_session(cls_model, spm_model, vocabulary):
@@ -31,7 +31,7 @@ def start_interactive_session(cls_model, spm_model, vocabulary):
         print('Sentence:', end=' ')
         sentence = input()
         pieces = spm_model.encode_as_pieces(sentence)
-        features = utils.to_integer_matrix([pieces], vocabulary).to(device)
+        features = utils.to_integer_matrix([pieces], vocabulary, padding='first').to(device)
         y_pred = torch.softmax(cls_model(features), dim=1)
 
         pred_str = ', '.join(f'{e * 100:.1f}' for e in y_pred[0])
@@ -46,9 +46,12 @@ def main():
     vocab = utils.read_vocabulary(os.path.join(spm_path, 'spm.vocab'))
 
     cls_path = os.path.join(out_path, 'sent/model.pth')
-    cls_model = models.GRUClassifier(
-        vocab_size=len(vocab), num_classes=2, embedding_dim=8, hidden_size=16) \
-        .to(utils.to_device())
+    # cls_model = models.TransformerClassifier(
+    #     vocab_size=len(vocab), num_classes=2, embedding_dim=8) \
+    #     .to(utils.to_device())
+    cls_model = models.RNNClassifier(
+        vocab_size=len(vocab), num_classes=2, embedding_dim=8, hidden_size=16,
+        cell_type='gru').to(utils.to_device())
 
     if not os.path.exists(cls_path):
         train_classifier(cls_model, spm_model, vocab)
